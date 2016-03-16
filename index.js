33f3735f39f94fc34d596a40e9001743b52df9b9
Mike <ellokopoko@gmail.com>
2016-03-16 21:48:08 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+  Your module code here
+
+  example:
+
+  export default class MyNewModule {
+    constructor() {
+    }
+  }
+
+  or
+
+  class MyNewModule {
+     constructor() {
+     }
+   }
+
+   module.exports = MyNewModule
+
+*/
---
+++ spec.js
@@ -0,0 +1,17 @@
+/*
+
+import MyNewModule from './index.js'
+or
+const MyNewModule = require('./index.js');
+const module = new MyNewModule();
+
+
+describe("Test MyNewModule", () => {
+
+    it("first test", () => {
+        expect(true).toBe(true);
+    });
+
+});
+
+*/
