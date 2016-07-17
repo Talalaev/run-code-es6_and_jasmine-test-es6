183b95adf9fc55ae113d8882d979687613949345
Mike <ellokopoko@gmail.com>
2016-07-17 13:48:05 +0300

    add webpack loader babel

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 /*
   Your module code here
-
   example:
-
   export default class MyNewModule {
     constructor() {
     }
   }
-
   or
-
   class MyNewModule {
      constructor() {
      }
    }
-
    module.exports = MyNewModule
-
 */
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-    entry: "./spec.js",
+    entry: {
+		//"spec-compiled": "./spec.js", //раскоментировать для сбоки запускаемой в браузере (конфликтует с build, нельзя подключать точку входа как модуль)
+		build: "./index.js"
+	},
     output: {
         path: __dirname,
-        filename: "bundle.js"
-    }
+        filename: "[name].js"
+    },
+	module: {
+	  loaders: [
+		{
+		  test: /\.js$/,
+		  loader: 'babel?presets[]=es2015'
+		}
+	  ]
+	}
 };
